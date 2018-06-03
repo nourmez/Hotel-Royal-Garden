25e1e27431ca76826b55755af89989d05e1fa23a
ismailmezrani <nourhene.mezrani@gmail.com>
2018-06-03 19:56:52 +0200

    hebergement

--- model/hfct.js
+++ model/hfct.js
@@ -8,3 +8,28 @@ function getName()
     });
 
 }
+function getLesReservtionsRestauration(callback) {
+
+    $query= "SELECT `nomClient`, `prenomClient`, `nbLit`, `nbTable`, `nbCouvert`\n" +
+        "FROM `reservation`, `chambre`, `client`, `restaurant`\n" +
+        "WHERE `chambre`.`id_reservation` = `reservation`.`id_reservation`\n" +
+        "AND `restaurant`.`id_reservation` = `reservation`.`id_reservation`\n" +
+        "AND `client`.`id_client` = `reservation`.`id_client`\n" +
+        "AND DATE(now()) = `reservation`.`dateDebut`";
+
+    bdd.connection.query($query, function (err, rows, fields) {
+
+        if (err) {
+            console.log("Problème de récupération des données des réservations.");
+            console.log(err);
+            return;
+        }else{
+            callback(null,rows);
+            console.log(fields);
+        }
+
+        console.log("Récupération des réservations avec succés !");
+    });
+
+    bdd.connection.end();
+}
